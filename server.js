5e0a71be69bc94e912502e4183f51a78685ac950
zntb <zntbdev@proton.me>
2025-10-17 07:40:00 +0300

    Fix websocket server

--- server.js
+++
@@ -1,6 +0,0 @@
-// WebSocket server for development
-// Run this with: node server.js
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const { createWebSocketServer } = require('./lib/websocket-server');
-
-createWebSocketServer(3001);
